a712390506a4339d9639b66e35a120d3350d43e0
Vhmit <praxedes.9630@gmail.com>
2023-05-22 23:31:43 -0300

    aljeter: Change dalvik to 3GB standard for smooth
    Signed-off-by: Vhmit <praxedes.9630@gmail.com>

--- properties.mk
+++ properties.mk
@@ -74,10 +74,10 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapstartsize=16m \
-    dalvik.vm.heapgrowthlimit=256m \
+    dalvik.vm.heapgrowthlimit=192m \
     dalvik.vm.heapsize=512m \
     dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=4m \
+    dalvik.vm.heapminfree=2m \
     dalvik.vm.heapmaxfree=8m
 
 # Debug
